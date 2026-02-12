9df79a4bf2ba03cd3e84680e5c7542e4992f3bab
StackOverLord27 <btech10621.23@bitmesra.ac.in>
2026-02-12 22:43:59 +0530

    Commit after changing the core OS of the project

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler(async (req, res)=> {
       throw new ApiError(400, "All fields are required")
    }
 
-   const existedUser= User.findOne({
+   const existedUser= await User.findOne({
       $or: [{username}, {email}]
    })
 
@@ -31,7 +31,9 @@ const registerUser = asyncHandler(async (req, res)=> {
    }
 
    const avatarLocalPath= req.files?.avatar[0]?.path;
-   const coverImageLocalPath= req.files?.coverImage[0]?.path;
+   let coverImageLocalPath;
+   if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0)
+      coverImageLocalPath= req.files.coverImage[0].path
 
    if(!avatarLocalPath){
       throw new ApiError(400, "Avatar is required")
@@ -44,22 +46,24 @@ const registerUser = asyncHandler(async (req, res)=> {
       throw new ApiError(400, "Avatar still missing")
    }
 
-   await User.create({
+   const user= await User.create({
       fullName,
       avatar: avatar.url,
       coverImage: coverImage?.url || "",
+      email,
       password,
       username: username.toLowerCase()
    })
 
-   const createUser=await User.findById(User._id).select("-password -refreshToken")
+   const createUser=await User.findById(user._id).select("-password -refreshToken")
+
 
    if(!createUser){
       throw new ApiError(500, "User Registration Unsuccessful!")
    }
 
    return res.status(201).json(
-      new ApiResponse(200, createdUser, "User Registered Successfully")
+      new ApiResponse(200, createUser, "User Registered Successfully")
    )
  })
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ const uploadFiles= async(localFilePath) => {
         const response= await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        console.log("File has been uploaded succesfully", response.url);
+        // console.log("File has been uploaded succesfully", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch(error){
         fs.unlinkSync(localFilePath) //remove the locally saved temp file as the upload got failed.
